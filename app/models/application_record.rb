7e625f308e352b540769217b796e85e86bad0233
Eduardo Rocha <eduardo.vinicius.rocha@gmail.com>
2026-06-20 11:19:11 -0300

    feat(db): database.yml com primary (rota_app) e admin (rota_admin)

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  self.abstract_class = true
+
+  connects_to database: {
+    writing: :primary,
+    reading: :primary,
+    admin:   :admin
+  }
 end
