335e9c62a68d36e3d635534ca392f4f022b1d213
Eduardo Rocha <eduardo.vinicius.rocha@gmail.com>
2026-06-20 11:31:38 -0300

    feat(core): Current.municipality_id (ADR-0019)

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,4 +1,7 @@
+# CurrentAttributes resetado por request e por job (ver ADR-0019, ADR-0020).
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
+  attribute :municipality_id
+
   delegate :user, to: :session, allow_nil: true
 end
